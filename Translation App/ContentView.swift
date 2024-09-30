6c934ed0fc90d0fb763b8420c84ac2e6855c40a1
nickatto <nickhaspeed@gmail.com>
2024-09-30 18:39:56 -0400

    Initial Commit

---
+++ Translation App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Translation App
+//
+//  Created by Nicholas Voronetskis on 9/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Translation App/Translation_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Translation_AppApp.swift
+//  Translation App
+//
+//  Created by Nicholas Voronetskis on 9/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Translation_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
